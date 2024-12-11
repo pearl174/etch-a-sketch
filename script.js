223cacfb117ee49c86b954b18b61303275ecd7cd
pearl-174 <115370971+pearl174@users.noreply.github.com>
2024-12-11 09:55:58 +0530

    Made the grid using flexbox (important: flex-direction and setting heights and widths of the container). Add event listener to each cell

---
+++ script.js
@@ -0,0 +1,19 @@
+let div = null;
+const container = document.querySelector("#container");
+let flexContainer = null;
+
+for (let i = 0; i < 4; i++) {
+    flexContainer = document.createElement("div");
+    flexContainer.classList.add("flexContainer");
+    for (let j = 0; j < 4; j++) {
+        div = document.createElement("div");
+        div.classList.add("cell");
+        div.addEventListener("mouseover", addColor);
+        flexContainer.appendChild(div);
+    }
+    container.appendChild(flexContainer);
+}
+
+function addColor() {
+    console.log("are things working?");
+}
