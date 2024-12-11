05c4b2e543826e2aaded46979e54cb7bb672ebc0
pearl-174 <115370971+pearl174@users.noreply.github.com>
2024-12-11 10:17:51 +0530

    Add change grid size logic to the button

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-let div = null;
-const container = document.querySelector("#container");
-let flexContainer = null;
+function createGrid(gridSize) {
+    let div = null;
+    const container = document.querySelector("#container");
+    let flexContainer = null;
 
-for (let i = 0; i < 4; i++) {
-    flexContainer = document.createElement("div");
-    flexContainer.classList.add("flexContainer");
-    for (let j = 0; j < 4; j++) {
-        div = document.createElement("div");
-        div.classList.add("cell");
-        div.addEventListener("mouseover", addColor);
-        flexContainer.appendChild(div);
+    for (let i = 0; i < gridSize; i++) {
+        flexContainer = document.createElement("div");
+        flexContainer.classList.add("flexContainer");
+        for (let j = 0; j < gridSize; j++) {
+            div = document.createElement("div");
+            div.classList.add("cell");
+            div.addEventListener("mouseover", addColor);
+            flexContainer.appendChild(div);
+        }
+        container.appendChild(flexContainer);
     }
-    container.appendChild(flexContainer);
 }
 
 function addColor(e) {
     console.log(e.target);
     e.target.classList.add("color");
 }
+
+function deleteGrid() {
+    const container = document.querySelector("#container");
+    while(container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+// button logic
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    const size = prompt("Number of squares per side: ");
+    deleteGrid();
+    createGrid(size);
+});
+
+createGrid(4);
